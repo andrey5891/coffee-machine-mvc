901227d5ec676c12e57dd6a26ba10f4a45da651f
Korchenkov <kc@kode.ru>
2023-03-16 21:03:13 +0300

    third commit

--- src/main/java/CoffeeMachine.java
+++ src/main/java/CoffeeMachine.java
@@ -3,6 +3,7 @@ import services.CoffeeMachineService;
 public class CoffeeMachine {
 
     //test
+    //third
     public static void main(String[] args) {
         new CoffeeMachineService().start();
     }
